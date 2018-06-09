ad5e9667375a1eb4d438ba9a6d45856da3024057
Xoadra <xijp77@gmail.com>
2018-06-08 19:45:43 -0700

    Email form created on contact page.

--- view/app/contact/contact.js
+++ view/app/contact/contact.js
@@ -11,13 +11,29 @@ import './contact.css'
 
 class Contact extends Component {
 	
+	onSend( event ) {
+		event.preventDefault( )
+		console.log( 'Nothing submitted!' )
+	}
+	
 	render( ) {
 		return (
 			<section>
 				<header>
 					<Navigator/>
 				</header>
-				<p> Contact </p>
+				<form onSubmit={ event => this.onSend( event ) }>
+					<h3 className="contact-email"> email@website.com </h3>
+					<label>
+						Subject
+						<input className="contact-field" name="title"/>
+					</label>
+					<label>
+						Message
+						<textarea className="contact-field" name="message"/>
+					</label>
+					<button className="contact-submit"> Send </button>
+				</form>
 			</section>
 		)
 	}
