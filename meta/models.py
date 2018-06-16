a7a375b9dddd4cac5f10b620a6eedf4a753b5dc1
Xoadra <xijp77@gmail.com>
2018-06-16 16:22:17 -0700

    Editing images now operating correctly.

--- meta/models.py
+++ meta/models.py
@@ -8,26 +8,34 @@ from django.db.models import Model, CharField, ImageField
 
 
 class Image( Model ):
-	name = CharField( max_length = 100, blank = False )
-	image = ImageField( blank = False )
+	name = CharField( max_length = 100 )
+	image = ImageField( )
 	
 	def __str__( self ):
 		return self.name
 	
 	def save( self, *args, **kwargs ):
-		## Grabs the incorrect image url when the image is new
-		oldurl = self.image.url
+		previous = Image.objects.get( pk = self.pk ) if self.pk else None
 		## Use the name field input as the new image file name
 		filename, extension = path.splitext( self.image.name )
 		imagename = 'img/' + self.name + extension
-		## Edit the existing image file once it is properly identified
-		if path.isfile( self.image.url[ 1: ] ):
+		## Get the previous, next, and uploaded urls respectively
+		preurl = '/root/img/' + previous.name + extension if previous else ''
+		nexturl = '/root/' + imagename
+		loadurl = self.image.url
+		## Edit the existing image file to reflect the name change
+		if path.isfile( self.image.url[ 1: ] ) and self.image.name != imagename:
 			self.image.save( imagename, self.image, save = False )
+			remove( loadurl[ 1: ] )
+		## Replace the old image with the updated image version
+		elif not path.isfile( self.image.url[ 1: ] ) and path.isfile( nexturl[ 1: ] ):
+			remove( nexturl[ 1: ] )
+			self.image.save( imagename, self.image, save = False )
+		## Invoke the superclass save method with a new path
 		self.image.name = imagename
-		## Invoke the saving operations from the inherited class
 		super( Image, self ).save( *args, **kwargs )
-		## Delete the old file since the newer version replaces it
-		## Only works properly if the name is all that is changed
-		remove( oldurl[ 1: ] )
+		## Trash the old file for one with a new name and image
+		if path.isfile( preurl[ 1: ] ) and preurl != nexturl:
+			remove( preurl[ 1: ] )
 
 
