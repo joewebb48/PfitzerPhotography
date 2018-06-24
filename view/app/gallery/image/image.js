235abe99135420a041b798c7ef163cc64d9d631b
Xoadra <xijp77@gmail.com>
2018-06-24 14:21:21 -0700

    Display toggling incorporated into Image component.

--- view/app/gallery/image/image.js
+++ view/app/gallery/image/image.js
@@ -10,13 +10,34 @@ import './image.css'
 
 class Image extends Component {
 	
+	constructor( props ) {
+		super( props )
+		this.state = { isOpen: false }
+	}
+	
+	
+	togglePanel( ) {
+		this.state.isOpen ? this.setState( { isOpen: false } ) : this.setState( { isOpen: true } )
+	}
+	
+	openPanel( ) {
+		if ( this.state.isOpen ) {
+			return (
+				<h3 className="image-text">
+					{ this.props.image.name }
+				</h3>
+			)
+		}
+	}
+	
 	render( ) {
 		const url = 'root/' + this.props.image.image
 		console.log( url )
 		console.log( this.props.image )
 		return (
-			<div className="image-frame">
+			<div className="image-frame" onClick={ ( ) => this.togglePanel( ) }>
 				<img className="image-thumbnail" src={ url }/>
+				{ this.openPanel( ) }
 			</div>
 		)
 	}
@@ -25,5 +46,6 @@ class Image extends Component {
 
 
 export default Image
+
 
 
