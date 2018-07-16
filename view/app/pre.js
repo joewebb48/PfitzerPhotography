9ae384bd937677041c20701f8a0be2388b4e2f06
Xoadra <xijp77@gmail.com>
2018-07-16 14:53:06 -0700

    Server-rendered app Redux and store added.

--- view/app/pre.js
+++ view/app/pre.js
@@ -4,18 +4,26 @@
 
 import React, { Component } from 'react'
 import { StaticRouter } from 'react-router-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 
 import Router from './router'
 
+
+
+const nexus = createStore( reducers, {  }, applyMiddleware( thunk ) )
 
 
 class App extends Component {
 	
 	render( ) {
 		return (
-			<StaticRouter location={ this.props.url } context={ {  } }>
-				<Router/>
-			</StaticRouter>
+			<Provider store={ nexus }>
+				<StaticRouter location={ this.props.url } context={ {  } }>
+					<Router/>
+				</StaticRouter>
+			</Provider>
 		)
 	}
 	
