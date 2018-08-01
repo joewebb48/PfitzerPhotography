06f4ccf10c0632a1d630dfae7590efb7fe227c29
Xoadra <xijp77@gmail.com>
2018-08-01 02:35:24 -0700

    Hovered image panels no longer clip.

--- view/app/components/gallery/gallery.js
+++ view/app/components/gallery/gallery.js
@@ -16,81 +16,35 @@ class Gallery extends Component {
 	
 	constructor( props ) {
 		super( props )
-		this.state = { images: [ ], view: null }
-		this.viewHover = this.viewHover.bind( this )
-		this.moveOff = this.moveOff.bind( this )
+		this.state = { images: [ ] }
 	}
 	
 	
 	componentDidMount( ) {
 		axios.get( '/photos' ).then( images => {
 			this.setState( { images: images.data } )
-			// Generate a unique css id selector for each image component
-			this.applyLevels( )
+			// Generate a unique scaling css selector per image component
+			this.generateLevels( )
 		} )
 	}
 	
-	populateFrame( ) {
-		let rank = 1
+	viewGallery( ) {
 		return this.state.images.map( image => {
-			let props = { rk: rank++, image: image.fields }
-			let events = { viewHover: this.viewHover, moveOff: this.moveOff }
-			return <Image key={ image.pk } { ...props } { ...events }/>
+			return <Image key={ image.pk } image={ image.fields }/>
 		} )
 	}
 	
-	viewHover( frame ) {
-		if ( !this.state.view ) {
-			console.log( '\nHovering!', frame.props.rk )
-			frame.setState( { superiorView: true } )
-			this.setState( { view: frame } )
-		}
-		// Old code preserved for viewing multiple panels simultaneously
-		/* else {
-			if ( frame.state.viewMode === 'view' ) {
-				console.log( '\nHovering with new!', frame.props.rk, this.state.view.props.rk )
-				if ( frame.props.rk < this.state.view.props.rk && this.state.view.state.superiorView ) {
-					this.state.view.setState( { superiorView: false } )
-				}
-			}
-			if ( frame !== this.state.view && frame.state.superiorView === this.state.view.state.superiorView ) {
-				console.log( 'Now:', frame.props.rk, 'Then:', this.state.view.props.rk )
-				let superior = frame.props.rk < this.state.view.props.rk
-				frame.setState( { superiorView: superior ? true : false } )
-				this.state.view.setState( { superiorView: superior ? false : true } )
-				this.setState( { view: frame } )
-			}
-		} */
-	}
-	
-	moveOff( frame ) {
-		console.log( 'Leaving!', frame.props.rk, this.state.view ? this.state.view.props.rk : null )
-		if ( frame === this.state.view ) {
-			setTimeout( ( ) => {
-				if ( frame === this.state.view && frame.state.viewMode === 'hide' ) {
-					console.log( 'Undone!', frame.props.rk, '\n' )
-					this.setState( { view: null } )
-				}
-				else {
-					console.log( 'Viewing another!', this.state.view.props.rk )
-				}
-			}, 500 )
-			console.log( 'Waiting...' )
-		}
-	}
-	
-	applyLevels ( ) {
-		const node = ReactDOM.createPortal( <></>, document.styleSheets[ 0 ].ownerNode )
-		const css = node.containerInfo.sheet
-		console.log( css )
+	generateLevels ( ) {
+		const css = ReactDOM.createPortal( <></>, document.styleSheets[ 0 ].ownerNode )
+		const styles = node.containerInfo.sheet
 		this.state.images.forEach( ( img, idx ) => {
 			let iterator = '.image-box:nth-last-child( ' + ( idx + 1 ) + ' )'
 			let zidstyle = iterator + ' { z-index: ' + ( idx + 4 ) + '; }'
-			css.insertRule( zidstyle, css.cssRules.length )
+			styles.insertRule( zidstyle, styles.cssRules.length )
 			// Alternative method for embedding new css stylesheet rules
 			/* let select = zidstyle.slice( 0, rank.toString( ).length + 2 )
 			let primacy = zidstyle.slice( rank.toString( ).length + 5, zidstyle.length - 2 )
-			css.addRule( select, primacy, css.cssRules.length ) */
+			styles.addRule( select, primacy, styles.cssRules.length ) */
 		} )
 	}
 	
@@ -101,7 +55,7 @@ class Gallery extends Component {
 					<Navigator/>
 				</header>
 				<div className="gallery-frame">
-					{ this.populateFrame( ) }
+					{ this.viewGallery( ) }
 				</div>
 			</section>
 		)
--- view/app/components/gallery/image/image.js
+++ view/app/components/gallery/image/image.js
@@ -12,48 +12,41 @@ class Image extends Component {
 	
 	constructor( props ) {
 		super( props )
-		this.state = { superiorView: false, viewMode: 'hide' }
+		this.state = { viewMode: 'hide' }
 		this.elevateUp = this.elevateUp.bind( this )
 		this.fallBack = this.fallBack.bind( this )
 	}
 	
 	
-	componentDidUpdate( ) {
-		if ( this.state.viewMode === 'view' ) {
-			this.props.viewHover( this )
-		}
-		else if ( this.state.superiorView && this.state.viewMode === 'hide' ) {
-			this.setState( { superiorView: false } )
-		}
-	}
-	
-	animateMode( element, prefix, suffix ) {
-		const alpha = element + ' ' + element + '-' + prefix
-		const beta = element + ' ' + element + '-' + suffix
-		if ( this.state.viewMode !== 'hide' ) {
-			return this.state.superiorView ? alpha : beta
-		}
-		return element
-	}
-	
 	elevateUp( ) {
-		this.setState( { viewMode: 'view' } )
-		this.props.viewHover( this )
+		if ( this.state.viewMode !== 'fade' ) {
+			this.setState( { viewMode: 'view' } )
+		}
+		else if ( this.state.viewMode === 'fade' ) {
+			// Prevent panel snapping from quickly moving back
+			setTimeout( ( ) => {
+				this.setState( { viewMode: 'view' } )
+			}, 400 )
+		}
 	}
 	
 	fallBack( ) {
-		setTimeout( ( ) => {
-			this.setState( { superiorView: false, viewMode: 'hide' } )
-		}, 500 )
-		this.setState( { viewMode: 'fade' } )
-		this.props.moveOff( this )
+		if ( this.state.viewMode !== 'hide' ) {
+			setTimeout( ( ) => {
+				// Disallow panel hiding if it is being viewed again
+				if ( this.state.viewMode !== 'view' ) {
+					this.setState( { viewMode: 'hide' } )
+				}
+			}, 500 )
+			this.setState( { viewMode: 'fade' } )
+		}
 	}
 	
 	setPanel( ) {
 		const price = this.props.image.price ? 'Price: $' + this.props.image.price : null
 		const date = this.props.image.date ? 'Date: ' + this.props.image.date : null
 		const animation = this.state.viewMode === 'view' ? 'image-panel-view' : 'image-panel-fade'
-		if ( this.state.viewMode !== 'hide' && this.state.superiorView ) {
+		if ( this.state.viewMode !== 'hide' ) {
 			return (
 				<div className={ 'image-panel ' + animation }>
 					<h3 className="image-title"> { this.props.image.name } </h3>
@@ -69,9 +62,8 @@ class Image extends Component {
 	render( ) {
 		const url = 'root/' + this.props.image.image
 		const events = { onMouseEnter: this.elevateUp, onMouseLeave: this.fallBack }
-		const style = this.animateMode( 'image-box', 'upper', 'lower' )
 		return (
-			<div id={ this.props.rk } className="image-box" { ...events }>
+			<div className="image-box" { ...events }>
 				<div className="image-shadow"></div>
 				<div className="image-frame">
 					<div className="image-border"></div>
