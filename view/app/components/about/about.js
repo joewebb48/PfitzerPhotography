ebc236bb4fcb1494dde99f24aed2730130d5ca11
Xoadra <xijp77@gmail.com>
2018-08-24 02:49:50 -0700

    About page image blending made more cross-browser friendly.

--- view/app/components/about/about.js
+++ view/app/components/about/about.js
@@ -28,24 +28,24 @@ class About extends Component {
 		const name = this.state.artist ? this.state.artist.fields.name : null
 		const about = this.state.artist ? this.state.artist.fields.about : null
 		// Needs additional style tinkering before image displays properly
-		const portrait = this.state.artist ? this.state.artist.fields.portrait : null
+		const portrait = this.state.artist ? this.state.artist.fields.portrait : ''
 		return (
 			<section className="about-section">
 				<div className="about-border"/>
 				<div id="about-portrait" className="about-frame">
 					<h1> { name } </h1>
 					<div className="about-image">
-						<img /* src={ '/public/img/' + portrait } *//>
+						<img src={ '/public/img/' + portrait }/>
 						<div className="about-upper"/>
 					</div>
 				</div>
-				{ /* <div className="about-distort"/> */ }
-				<div id="about-biography" className="about-frame">
-					<div className="about-text">
-						<p> { about } </p>
-					</div>
+				<div className="about-distort"/>
+				<div className="about-text">
+					<p> { about } </p>
+				</div>
+				<div id="about-background" className="about-frame">
 					<div className="about-image">
-						<img/>
+						<img src="/public/img/test1.jpg"/>
 						<div className="about-lower"/>
 					</div>
 				</div>
