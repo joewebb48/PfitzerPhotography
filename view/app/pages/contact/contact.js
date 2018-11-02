7f13219800f7efb090c9332319d185c464255520
Xoadra <xijp77@gmail.com>
2018-11-01 21:42:51 -0700

    Email address field added to contact form.

--- view/app/pages/contact/contact.js
+++ view/app/pages/contact/contact.js
@@ -22,8 +22,8 @@ class Contact extends Component {
 		super( props )
 		const field = { value: '', error: '' }
 		const chrono = { max: 12000, hide: 2000 }
-		const preset = { title: field, email: field }
 		const status = { sent: false, diff: chrono }
+		const preset = { title: field, email: field, content: field }
 		this.state = { active: false, form: { ...preset, status } }
 		this.updateForm = this.updateForm.bind( this )
 	}
@@ -47,8 +47,9 @@ class Contact extends Component {
 		const info = { ...this.state.form[ name ], value }
 		const draft = { ...this.state.form, [ name ]: info }
 		// Update contact form with new form field input data
-		const ready = draft.title.value && draft.email.value
-		this.setState( { form: draft, active: ready } )
+		const key = field => field[ 0 ] === 'status' || field[ 1 ][ 'value' ]
+		const unlock = Object.entries( draft ).every( key )
+		this.setState( { form: draft, active: unlock } )
 	}
 	
 	onSend( event ) {
@@ -58,13 +59,13 @@ class Contact extends Component {
 	}
 	
 	validateInput( form ) {
-		const floor = { title: 10, email: 100 }
+		const floor = { title: 10, email: 10, content: 100 }
+		const subs = { title: 'subject', email: 'email', content: 'message' }
 		const amalgam = { status: { ...form.status, sent: true } }
 		for ( let field in form ) {
 			if ( field !== 'status' ) {
-				let type = field === 'title' ? 'subject' : 'message'
 				let valid = form[ field ].value.length >= floor[ field ]
-				let warn = valid ? '' : 'Your ' + type + ' is too short!'
+				let warn = valid ? '' : 'Your ' + subs[ field ] + ' is too short!'
 				amalgam[ field ] = { ...form[ field ], error: warn }
 			}
 		}
@@ -72,16 +73,18 @@ class Contact extends Component {
 	}
 	
 	render( ) {
-		const { title, email, status } = this.state.form
+		const { title, email, content, status } = this.state.form
 		const lock = { disabled: !this.state.active }
-		const user = this.props.biography.fields ? this.props.biography.fields.name : ''
+		const noun = this.props.biography.fields ? this.props.biography.fields.name : ''
 		// Set controlled component architecture in form fields
 		const head = { name: 'title', placeholder: 'Your message subject.', ...title, ...status }
-		const body = { name: 'email', placeholder: 'Your message here.', ...email, ...status }
+		const user = { name: 'email', placeholder: 'Your email address here.', ...email, ...status }
+		const body = { name: 'content', placeholder: 'Your message here.', ...content, ...status }
 		return (
 			<form onSubmit={ event => this.onSend( event ) }>
-				<h3 className="contact-form"> Contact { user.split( ' ' )[ 0 ] } </h3>
+				<h3 className="contact-form"> Contact { noun.split( ' ' )[ 0 ] } </h3>
 				<Label html="input" onChange={ this.updateForm } { ...head }/>
+				<Label html="input" onChange={ this.updateForm } { ...user }/>
 				<Label html="textarea" onChange={ this.updateForm } { ...body }/>
 				<button className="contact-submit" { ...lock }> Send </button>
 			</form>
@@ -92,6 +95,5 @@ class Contact extends Component {
 
 
 export default connect( mapBiography, { getBiography } )( Contact )
-
 
 
