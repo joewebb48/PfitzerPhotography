94187322b8e300954e1ff32949f5cc024673f99c
Xoadra <xijp77@gmail.com>
2018-12-02 23:51:12 -0800

    Successfully connected new MySQL database.

--- setup/settings.py
+++ setup/settings.py
@@ -94,6 +94,12 @@ DATABASES = {
 	'default': {
 		'ENGINE': 'django.db.backends.sqlite3',
 		'NAME': os.path.join( BASE_DIR, 'db.sqlite3' )
+	},
+	'mysql': {
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'pfitzer_photography',
+		'USER': vault.environ.get( 'USER' ),
+		'PASSWORD': vault.environ.get( 'PASSWORD' )
 	}
 }
 
